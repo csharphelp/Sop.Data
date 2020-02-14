2cc867408c3e54c0eb8e1d07c63e62c112722199
guojiaqiu <sopcce@qq.com>
2020-02-14 23:11:30 +0800

    啊大大

---
+++ tools/Web/Configure/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Configure
+{
+    /// <summary>
+    /// 配置文件
+    /// </summary>
+    public class AppSettings
+    {
+        public RedisCaching RedisCaching { get; set; }
+    }
+    /// <summary>
+    /// Redis
+    /// </summary>
+    public class RedisCaching
+    {
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// 链接信息
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
